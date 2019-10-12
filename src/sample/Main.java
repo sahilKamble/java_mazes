9a582d83a9e24cd511042d08cac8f0cad0903b62
Genos2000 <paraskamble2000@gmail.com>
2019-10-12 23:57:25 +0530

    removed unused imports and fixed maze loop

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,48 +1,43 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        String s = new MazeGenerator(20,20).toString();
+        String s = new MazeGenerator(20, 20).toString();
         int n = s.length();
+        System.out.println(s);
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         Rectangle rect[] = new Rectangle[n];
-        int k=0;
-        int j=0;
-        for(int i = 0; i < n; i++) {
+        int k = 0;
+        int j = 0;
+        for (int i = 0; i < n; i++) {
             rect[i] = new Rectangle();
             rect[i].setWidth(10);
             rect[i].setHeight(10);
-            if(s.charAt(i) == '0') {
-                rect[i].setFill(Color.SLATEBLUE);
-            } else if (s.charAt(i) == '1'){
+            if (s.charAt(i) == '0') {
+                rect[i].setFill(Color.WHITE);
+            } else if (s.charAt(i) == '1') {
                 rect[i].setFill(Color.BLACK);
             } else if (s.charAt(i) == '\n') {
+                GridPane.setConstraints(rect[i], j++, k);
                 j = 0;
                 k++;
+                continue;
             }
             GridPane.setConstraints(rect[i], j++, k);
         }
         grid.getChildren().addAll(rect);
         primaryStage.setTitle("Maze");
-        primaryStage.setScene(new Scene(grid, 800, 800));
+        primaryStage.setScene(new Scene(grid, 500, 500));
         primaryStage.show();
     }
 
--- src/sample/MazeGenerator.java
+++ src/sample/MazeGenerator.java
@@ -1,6 +1,5 @@
 package sample;
 
-import java.util.*;
 import java.util.LinkedList;
 import java.util.Random;
 
